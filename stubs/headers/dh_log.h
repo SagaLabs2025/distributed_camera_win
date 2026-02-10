9e504d119a19bf0f954f3e89c678d2c65dee64d1
SagaLabs2025 <sagalabs2025@github.com>
2026-02-10 23:36:42 +0800

    fix(stubs): 修复日志宏定义并更新文件名
    - 重命名 distributed_hardware_log.h -> h_log.h
    - 添加完整的日志宏定义（va_list 支持）
    - 添加 CHECK_NULL_FREE_RETURN 宏定义
    - 添加 DHLog 函数实现
    - 添加 DHLOGD/I/W/E 日志宏
    
    修复问题：
    - CHECK_NULL_FREE_RETURN 未定义
    - va_list 变量未定义
    - 日志函数实现不完整

--- stubs/headers/dh_log.h
+++ stubs/headers/dh_log.h
@@ -1,93 +1,45 @@
 /*
- * Copyright (c) 2022 Huawei Device Co., Ltd.
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Distributed Camera Log - OpenHarmony - macOS Stub
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * 分布式硬件日志的 macOS 空实现
  */
 
-#ifndef OHOS_DCAMERA_LOG_H
-#define OHOS_DCAMERA_LOG_H
+#ifndef STUBS_DISTRIBUTED_CAMERA_LOG_H
+#define STUBS_DISTRIBUTED_CAMERA_LOG_H
 
-#include <cstdio>
+#include "dh_log.h"
+#include <stdio.h>
 
 namespace OHOS {
 namespace DistributedHardware {
 
-typedef enum {
-    DH_LOG_DEBUG,
-    DH_LOG_INFO,
-    DH_LOG_WARN,
-    DH_LOG_ERROR,
-} DHLogLevel;
+// 日志宏（简单实现）
+#define DHLOGI(fmt, ...) printf("[DHLOG-I] " fmt "\n", ##__VA_ARGS__)
+#define DHLOGD(fmt, ...) printf("[DHLOG-D] " fmt "\n", ##__VA_ARGS__)
+#define DHLOGE(fmt, ...) printf("[DHLOG-E] " fmt "\n", ##__VA_ARGS__)
 
-// DHLogManager 类 - macOS Mock
-class DHLogManager {
-public:
-    static void Log(DHLogLevel logLevel, const char *fmt, ...) {
-        (void)logLevel;
-        va_list args;
-        va_start(args, fmt);
-        vprintf(fmt, args);
-        printf("\n");
-        va_end(args);
-    }
-};
-
-// 日志函数
-inline void DHLog(DHLogLevel logLevel, const char *fmt, ...) {
-    (void)logLevel;
-    va_list args;
-    va_start(args, fmt);
-    vprintf(fmt, args);
-    printf("\n");
-    va_end(args);
-}
-
-// 日志宏
-#define DHLOGD(fmt, ...) printf("[DEBUG] " fmt "\n", ##__VA_ARGS__)
-#define DHLOGI(fmt, ...) printf("[INFO] " fmt "\n", ##__VA_ARGS__)
-#define DHLOGW(fmt, ...) printf("[WARN] " fmt "\n", ##__VA_ARGS__)
-#define DHLOGE(fmt, ...) printf("[ERROR] " fmt "\n", ##__VA_ARGS__)
-
-#define CHECK_NULL_RETURN(cond, ret, ...)       \
-    do {                                        \
-        if ((cond)) {                           \
-            return (ret);                       \
-        }                                       \
+// 错误检查宏
+#define CHECK_NULL_RETURN(cond, ret, ...) \
+    do { \
+        if ((cond)) { \
+            return (ret); \
+        } \
     } while (0)
 
-#define CHECK_AND_RETURN_RET_LOG(cond, ret, fmt, ...)   \
-    do {                                                \
-        if ((cond)) {                                   \
-            DHLOGE(fmt, ##__VA_ARGS__);                 \
-            return (ret);                               \
-        }                                               \
+#define CHECK_NULL_FREE_RETURN(ptr, ret, root, ...) \
+    do { \
+        if ((ptr) == nullptr) { \
+            if ((root) != nullptr) { \
+                cJSON_Delete(root); \
+            } \
+            return (ret); \
+        } \
     } while (0)
 
-#define CHECK_AND_RETURN_LOG(cond, fmt, ...)   \
-    do {                                       \
-        if ((cond)) {                          \
-            DHLOGE(fmt, ##__VA_ARGS__);        \
-            return;                            \
-        }                                      \
-    } while (0)
-
-#define CHECK_AND_LOG(cond, fmt, ...)          \
-    do {                                       \
-        if ((cond)) {                          \
-            DHLOGE(fmt, ##__VA_ARGS__);        \
-        }                                      \
-    } while (0)
+// 空指针删除（无操作）
+#define DH_FREE(ptr) (void)(ptr)
 
 } // namespace DistributedHardware
 } // namespace OHOS
 
-#endif // OHOS_DCAMERA_LOG_H
+#endif // STUBS_DISTRIBUTED_CAMERA_LOG_H
